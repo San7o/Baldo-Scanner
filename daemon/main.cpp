63def22943a4e458825a023b7ed7169bb161be27
San7o <santigio2003@gmail.com>
2024-07-29 12:39:38 +0200

    Setup project

---
+++ daemon/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+  
+    // create a socket
+    // while(1) {
+    //    listen();
+    //    accept()
+    //    new thread to handle the connection
+    // }
+
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    stdenv.cc.cc.lib        # libc
+    cmake                   # build system
+    doxygen                 # documentation
+  ];
+
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:";
+}
